6e6d669d080d5b80bb14bffe882a587a691ba929
Krzysiek Stala <krzysiek.stala@gmail.com>
2023-04-25 08:33:17 +0200

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -34,10 +34,5 @@ module.exports = {
         ],
       },
     },
-    upload: {
-      target: "lhci",
-      serverBaseUrl: "https://lhci-server.apps.shopware.io",
-      token: "6192d5d3-055a-4e82-be64-41cc7434c3f9",
-    },
   },
 };
