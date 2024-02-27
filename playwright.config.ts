63d0881275c91263522a147f0d06e2938ab5489b
Krzysiek Stala <krzysiek.stala@gmail.com>
2024-02-27 17:02:39 +0100

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -80,13 +80,4 @@ export default defineConfig({
     //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm start',
-    url: 'http://127.0.0.1:8080',
-    reuseExistingServer: !process.env.CI,
-    stdout: 'ignore',
-    stderr: 'pipe',
-  },
 });
