96f214d2beb4ac4a0be48bb285ccad50e19f9448
Krzysiek Stala <krzysiek.stala@gmail.com>
2024-03-15 00:31:48 +0100

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "retain-on-failure",
-    baseUrl
+    baseURL,
   },
 
   /* Configure projects for major browsers */
