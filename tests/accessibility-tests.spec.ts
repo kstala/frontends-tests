ad0d2b3d33e7dca859766e306ed44d0cbcef58a6
Kstala <krzysiek.stala@gmail.com>
2024-04-10 13:49:51 +0200

    chore: small tests fixes

--- tests/accessibility-tests.spec.ts
+++ tests/accessibility-tests.spec.ts
@@ -4,9 +4,9 @@ import { createHtmlReport } from 'axe-html-reporter';
 const fs = require('fs');
 
 
-test.describe('homepage', () => { // 2
+test.describe('homepage', () => { 
   test('should not have any automatically detectable accessibility issues', async ({ page }, testInfo) => {
-    await page.goto('https://frontends-demo.vercel.app/'); // 3
+    await page.goto('https://frontends-demo.vercel.app/'); 
 
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
     const reportHTML = createHtmlReport({
@@ -22,18 +22,6 @@ test.describe('homepage', () => { // 2
       });
     }
     fs.writeFileSync("build/reports/homepage-report.html", reportHTML);
-
-  //Attached the violations to the test report
-  await testInfo.attach("accessibility-scan-results", {
-    body: JSON.stringify(accessibilityScanResults.violations, null, 2),
-    contentType: "application/json",
-  });
-
-  //Console log the violations
-  let violation = accessibilityScanResults.violations;
-  violation.forEach(function (entry) {
-    console.log(entry.impact + " " + entry.description);
-  });
 
   //Expect violations to be empty
   expect(accessibilityScanResults.violations).toEqual([]);
@@ -59,18 +47,6 @@ test.describe('product page', () => { // 2
     }
     fs.writeFileSync("build/reports/product-page-report.html", reportHTML);
 
-  //Attached the violations to the test report
-  await testInfo.attach("accessibility-scan-results", {
-    body: JSON.stringify(accessibilityScanResults.violations, null, 2),
-    contentType: "application/json",
-  });
-
-  //Console log the violations
-  let violation = accessibilityScanResults.violations;
-  violation.forEach(function (entry) {
-    console.log(entry.impact + " " + entry.description);
-  });
-
   //Expect violations to be empty
   expect(accessibilityScanResults.violations).toEqual([]);
 });
@@ -94,18 +70,6 @@ test.describe('category page', () => { // 2
       });
     }
     fs.writeFileSync("build/reports/category-page-report.html", reportHTML);
-
-  //Attached the violations to the test report
-  // await testInfo.attach("accessibility-scan-results", {
-  //   body: JSON.stringify(accessibilityScanResults.violations, null, 2),
-  //   contentType: "application/json",
-  // });
-
-  // //Console log the violations
-  // let violation = accessibilityScanResults.violations;
-  // violation.forEach(function (entry) {
-  //   console.log(entry.impact + " " + entry.description);
-  // });
 
   //Expect violations to be empty
   expect(accessibilityScanResults.violations).toEqual([]);
